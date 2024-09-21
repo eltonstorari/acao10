2cc8885bdebf22dc41b0f35fbdc079c14d902575
Elton Storari <eltonstorari@gmail.com>
2024-09-21 16:26:33 -0300

    Adicionado os recurso para iniciar a CRUD e consula dos parceiros

--- Acao10App/app/src/main/java/com/storaritech/acao10app/admin/ui/parceiro/ParceiroFragment.java
+++ Acao10App/app/src/main/java/com/storaritech/acao10app/admin/ui/parceiro/ParceiroFragment.java
@@ -7,11 +7,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.storaritech.acao10app.R;
+import com.storaritech.acao10app.entidades.Clube;
+import com.storaritech.acao10app.entidades.Parceiro;
+
+import java.util.ArrayList;
 
 
 public class ParceiroFragment extends Fragment {
+
+    RequestQueue request;
+    JsonObjectRequest jsonObjectReq;
+    ArrayList<Parceiro> listaParceiros;
+    EditText editNome, editCNPJ, editWhatsapp, editInstagram, editContato, editSite;
+    ImageView imgFoto;
+    TextView txt_parceiroAdmin_propaganda,  txt_parceiroAdmin_propagandaAtivas, txt_parceiroAdmin_cadastro;
+
     public ParceiroFragment() {
 
 }
@@ -22,8 +40,27 @@ public class ParceiroFragment extends Fragment {
                              Bundle savedInstanceState) {
         View vista = inflater.inflate(R.layout.fragment_parceiro, container, false);
 
+        editCNPJ = vista.findViewById(R.id.edit_parceiroAdmin_cnpj);
+        editNome = vista.findViewById(R.id.edit_parceiroAdmin_nome);
+        editWhatsapp = vista.findViewById(R.id.edit_parceiroAdmin_whatsapp);
+        editInstagram = vista.findViewById(R.id.edit_parceiroAdmin_instagram);
+        editContato = vista.findViewById(R.id.edit_parceiroAdmin_contato);
+        editSite = vista.findViewById(R.id.edit_parceiroAdmin_site);
+
+        imgFoto = vista.findViewById(R.id.img_parceriosAdmin_Foto);
+
+        txt_parceiroAdmin_propaganda = vista.findViewById(R.id.txt_parceiroAdmin_propaganda);
+        txt_parceiroAdmin_propagandaAtivas = vista.findViewById(R.id.txt_parceiroAdmin_propagandaAtivas);
+        txt_parceiroAdmin_cadastro = vista.findViewById(R.id.txt_parceiroAdmin_cadastro);
+
+        carregarWEBServiceListaParceiros();
 
 
         return vista;
+    }
+
+    private void carregarWEBServiceListaParceiros() {
+        String ip = getString(R.string.ip);
+        String url = ip + "/acao10/api/parceiros/consultarLista.php";
     }
 }
---
+++ Acao10App/app/src/main/java/com/storaritech/acao10app/entidades/Parceiro.java
@@ -0,0 +1,94 @@
+package com.storaritech.acao10app.entidades;
+
+public class Parceiro {
+
+            String cnpj, nome, cadastro, propaganda, propaganda_ativa, whatsapp, instagram, facebook, contato, site, url_logo;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCadastro() {
+        return cadastro;
+    }
+
+    public void setCadastro(String cadastro) {
+        this.cadastro = cadastro;
+    }
+
+    public String getPropaganda() {
+        return propaganda;
+    }
+
+    public void setPropaganda(String propaganda) {
+        this.propaganda = propaganda;
+    }
+
+    public String getPropaganda_ativa() {
+        return propaganda_ativa;
+    }
+
+    public void setPropaganda_ativa(String propaganda_ativa) {
+        this.propaganda_ativa = propaganda_ativa;
+    }
+
+    public String getWhatsapp() {
+        return whatsapp;
+    }
+
+    public void setWhatsapp(String whatsapp) {
+        this.whatsapp = whatsapp;
+    }
+
+    public String getInstagram() {
+        return instagram;
+    }
+
+    public void setInstagram(String instagram) {
+        this.instagram = instagram;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getUrl_logo() {
+        return url_logo;
+    }
+
+    public void setUrl_logo(String url_logo) {
+        this.url_logo = url_logo;
+    }
+}
