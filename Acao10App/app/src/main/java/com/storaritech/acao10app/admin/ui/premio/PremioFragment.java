60a0aaa9d2e4e57fdaa1a26516d5ff55bc0c08da
Elton Storari <eltonstorari@gmail.com>
2024-09-22 13:37:33 -0300

    Adicionado Designer da View premios

---
+++ Acao10App/app/src/main/java/com/storaritech/acao10app/admin/ui/premio/PremioFragment.java
@@ -0,0 +1,30 @@
+package com.storaritech.acao10app.admin.ui.premio;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.storaritech.acao10app.R;
+
+public class PremioFragment extends Fragment {
+
+
+    public PremioFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View vista = inflater.inflate(R.layout.fragment_premio, container, false);
+
+
+
+
+        return vista;
+    }
+}
