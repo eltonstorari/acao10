a4f42b90a55d078634f7eaf28f14de93b995703c
Elton Storari <eltonstorari@gmail.com>
2024-09-29 12:17:46 -0300

    Adicionando arquivo para merge em propagandas -> main

---
+++ Acao10App/app/src/main/java/com/storaritech/acao10app/admin/ui/propaganda/PropagandaFragment.java
@@ -0,0 +1,39 @@
+package com.storaritech.acao10app.admin.ui.propaganda;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.storaritech.acao10app.R;
+
+public class PropagandaFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public PropagandaFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View vista = inflater.inflate(R.layout.fragment_propaganda, container, false);
+
+
+
+
+        return vista;
+    }
+}
