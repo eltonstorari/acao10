11406836a16648a88d3e65436a02549d84a91712
Elton Storari <eltonstorari@gmail.com>
2024-10-13 11:49:33 -0300

    Adicionado barra de menu

--- Acao10App/app/build.gradle.kts
+++ Acao10App/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.storaritech.acao10app"
         minSdk = 24
-        targetSdk = 29
+        targetSdk = 27
         versionCode = 1
         versionName = "1.0"
 
